6c1f7127fa5ab18a1a0b30256dff8a3d7a77b0e4
12210675_Muhammad Nur <99000525+gothangleshineer@users.noreply.github.com>
2022-09-19 11:46:51 +0700

    modified:   app/Config/Routes.php 	new file:   app/Controllers/Page.php 	new file:   app/Views/Home.php 	new file:   app/Views/errors/About.php

---
+++ app/Controllers/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+
+class Page extends BaseController
+{
+     public function faq()
+     {
+          echo "HAlaman FaQ";
+     }
+
+     public function about()
+     {
+          echo "HAlaman AbOuT";
+     }
+}
---
+++ app/Views/errors/About.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>About</title>
+</head>
+
+<body>
+     <h1>About Me</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reprehenderit fuga sed ratione natus fugit est maxime, debitis excepturi ea distinctio minima velit quasi in quis hic, quibusdam iure voluptatibus.</p>
+</body>
+
+</html>
