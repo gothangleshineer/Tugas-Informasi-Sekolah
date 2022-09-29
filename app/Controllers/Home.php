f6d1d837c3030c65faf46339d2f8aaa86032fa3c
12210675_Muhammad Nur <99000525+gothangleshineer@users.noreply.github.com>
2022-09-29 16:24:20 +0700

    configurasi table mhs

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,6 +9,25 @@ class Home extends BaseController
         $this->load->model('m_mhs');
         $data['mahasiswa'] = $this->m_mhs->get_data();
 
-        $this->load->view('v_mhs', $data);
+        $this->load->view('Home', $data);
+    }
+    function halaman_email()
+    {
+        $email = \config\Services::email();
+        $alamat_email = "cyber8485@gmail.com";
+        $email->setTo($alamat_email);
+        $alamat_pengirim = "delusionboys074@gmail.com";
+        $email->setFrom($alamat_pengirim);
+        $subject = "test email";
+        $email->setSubject($subject);
+        $isi_pesan = "semoga selalu semangat";
+        $email->setMessage($isi_pesan);
+        if ($email->send()) {
+            echo "<h1>Pesan Terkirim</h1>";
+        } else {
+            echo "<h1>Pesan Gagal Terkirim</h1>";
+            $data_error = $email->printDebugger();
+            print_r($data_error);
+        }
     }
 }
